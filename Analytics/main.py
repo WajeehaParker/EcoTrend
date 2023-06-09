e36583f685e56a021ee41b8a8d924356c575562d
Wajeeha Parker <Wajeeha.Parker@tpsonline.com>
2023-06-09 14:08:12 +0500

    removing analytics container

--- Analytics/main.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-pool = redis.ConnectionPool(host='redis', port=6379, db=0)
-redis = redis.Redis(connection_pool=pool)
-
-########## fetching data from redis ##########
-
-value = redis.get("Values:AFE:2020:EG.ELC.ACCS.RU.ZS")
-print(value)
